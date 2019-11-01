406d95c2eb09f706e3d708fa79412991b03b6f71
Victor Arowo <arowovici@gmail.com>
2019-11-01 11:10:10 +0100

    Added Middleware and Routes for Actions

--- project/projectRouter.js
+++ project/projectRouter.js
@@ -3,12 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 const projectDb = require('../data/helpers/projectModel');
+const { validateProject, validateProjectId } = require('../middlewares');
 
 router.post('/', validateProject, (req, res) => {
-  const { name, desciption, completed } = req.body;
+  const { name, description, completed } = req.body;
 
   projectDb
-    .insert({ name, desciption, completed: completed || false })
+    .insert({ name, description, completed: completed || false })
     .then(project => {
       return res.status(201).json(project);
     })
@@ -48,7 +49,7 @@ router.get('/:id/actions', validateProjectId, (req, res) => {
 router.delete('/:id', validateProjectId, (req, res) => {
   const { id } = req.params;
 
-  userDb
+  projectDb
     .remove(id)
     .then(project => {
       return res.status(200).json(project);
@@ -60,10 +61,10 @@ router.delete('/:id', validateProjectId, (req, res) => {
 
 router.put('/:id', validateProjectId, validateProject, (req, res) => {
   const { id } = req.params;
-  const { name, desciption, completed } = req.body;
+  const { name, description, completed } = req.body;
 
   projectDb
-    .update(id, { name, desciption, completed: completed || false })
+    .update(id, { name, description, completed: completed || false })
     .then(user => {
       return res.status(200).json(user);
     })
@@ -71,42 +72,5 @@ router.put('/:id', validateProjectId, validateProject, (req, res) => {
       res.status(500).json({ message: 'Something went wrong' + err.message });
     });
 });
-
-//custom middleware
-
-function validateProjectId(req, res, next) {
-  const { id } = req.params;
-  if (Number(id) == id) {
-    projectDb
-      .get(id)
-      .then(project => {
-        if (project) {
-          req.project = project;
-          next();
-        } else {
-          return res.status(404).json({ message: 'Invalid project ID' });
-        }
-      })
-      .catch(err => {
-        return res
-          .status(500)
-          .json({ message: `The project could not be retrieved: ${err}` });
-      });
-  } else {
-    return res.status(400).json({ message: `Your ID is garbage` });
-  }
-}
-
-function validateProject(req, res, next) {
-  const { name, desciption } = req.body;
-
-  if (!Object.keys(req.body).length) {
-    return res.status(400).json({ message: 'You have not passed in a body' });
-  }
-  if (!name || !desciption || !completed) {
-    return res.status(400).json({ message: 'Your json is incomplete, scrub' });
-  }
-  next();
-}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 const projectRouter = require('./project/projectRouter');
+const actionsRouter = require('./actions/actionsRouter');
 
 const server = express();
 
@@ -11,6 +12,7 @@ server.use(express.json());
 server.use(cors());
 
 server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionsRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>This works!</h2>`);
